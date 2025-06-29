f64b31a8d7a59c2084f3fd1c675b04c038a51a41
RaghuvanshiSahab <166270874+RaghuvanshiSahab@users.noreply.github.com>
2025-06-29 17:49:35 +0530

    solving some bullshit errors and again launching it

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector(".flight").addEventListener("click", function(){
+    document.querySelector(".pop").style.display = "block"; 
+})
+document.querySelector(".pop i").addEventListener("click", function(){
+    document.querySelector(".pop").style.display = "none"; 
+})
